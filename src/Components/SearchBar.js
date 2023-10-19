975dd117f5bc5e036180939acd6007b8e84c14fd
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-10-19 22:23:43 +0700

    Add some hover transition for button in search bar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -5,36 +5,36 @@ import SearchIconSVG from "@/Assets/Icons/SVG/SearchIconSVG";
 
 const SearchBar = () => {
 	return (
-		<div className="absolute flex items-center justify-between gap-8 px-16 py-12 shadow-2xl bg-main-bg -bottom-16 inset-x-16 font-vietnam-pro rounded-2xl flex-nowrap">
-			<div className="flex items-start gap-3">
+		<div className="absolute flex items-center justify-between p-8 shadow-2xl bg-gradient-to-tr from-main-bg from-70% -bottom-16 inset-x-16 font-vietnam-pro rounded-2xl flex-nowrap">
+			<div className="flex items-start gap-3 px-8 py-8 transition-all duration-300 cursor-pointer hover:shadow-2xl rounded-2xl">
 				<NavigationSVG className="w-5 h-5 mt-1 -rotate-90 cursor-pointer fill-current text-sub-text"></NavigationSVG>
 				<div className="cursor-pointer">
 					<p className="text-2xl font-semibold">Địa điểm</p>
 					<p className="text-sm text-sub-text">Nơi bạn muốn đến </p>
 				</div>
 			</div>
-			<div className="flex items-start gap-3">
+			<div className="flex items-start gap-3 p-8 transition-all duration-300 cursor-pointer hover:shadow-2xl rounded-2xl">
 				<CalendarSVG className="w-5 h-5 mt-1 cursor-pointer fill-current text-sub-text"></CalendarSVG>
 				<div className="cursor-pointer">
 					<p className="text-2xl font-semibold">Nhận phòng</p>
 					<p className="text-sm text-sub-text">Thời gian nhận phòng</p>
 				</div>
 			</div>
-			<div className="flex items-start gap-3">
+			<div className="flex items-start gap-3 p-8 transition-all duration-300 cursor-pointer hover:shadow-2xl rounded-2xl">
 				<CalendarSVG className="w-5 h-5 mt-1 cursor-pointer fill-current text-sub-text"></CalendarSVG>
 				<div className="cursor-pointer">
 					<p className="text-2xl font-semibold">Trả phòng</p>
 					<p className="text-sm text-sub-text">Thời gian trả phòng</p>
 				</div>
 			</div>
-			<div className="flex items-start gap-3">
+			<div className="flex items-start gap-3 px-8 py-8 transition-all duration-300 cursor-pointer hover:shadow-2xl rounded-2xl">
 				<TravelerSVG className="w-6 h-6 cursor-pointer fill-current text-sub-text"></TravelerSVG>
 				<div className="cursor-pointer">
 					<p className="text-2xl font-semibold">Hành khách</p>
 					<p className="text-sm text-sub-text">Thêm số lượng hành khách</p>
 				</div>
 			</div>
-			<button className=" bg-[#3b71fe] p-4 rounded-full">
+			<button className=" bg-[#3b71fe] p-4 rounded-full hover:opacity-60 transition-opacity duration-200">
 				<SearchIconSVG className="w-5 h-5 text-white fill-current"></SearchIconSVG>
 			</button>
 		</div>
--- src/Layouts/HomePage/SearchArea.js
+++ src/Layouts/HomePage/SearchArea.js
@@ -1,4 +1,5 @@
 import homepage_bg from "@/Assets/Images/homepage_bg2.jpg";
+import homepage_bg2 from "@/Assets/Images/vietnam_crops_fields.jpg";
 import SearchBar from "@/Components/SearchBar";
 
 const SearchArea = () => {
@@ -6,7 +7,7 @@ const SearchArea = () => {
 		<div className=" h-[550px] relative mt-8">
 			<div
 				className="absolute inset-0 z-0 bg-center bg-no-repeat bg-cover rounded-2xl"
-				style={{ backgroundImage: `url(${homepage_bg})` }}
+				style={{ backgroundImage: `url(${homepage_bg2})` }}
 			></div>
 			<div className="absolute z-10 flex flex-col font-medium text-white font-vietnam-pro inset-y-20 inset-x-16">
 				<p className="text-[64px] ">

7fa5bd22e8688c224764716cd6c7102b2a260b9a
Thuan <20520795@gm.uit.edu.vn>
2023-10-21 00:48:41 +0700

    Change something UI in SearbarUI, update Selector UI, make event choose location in selection

--- src/Components/Selector.js
+++ src/Components/Selector.js
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
+import PathSVG from "@/Assets/Icons/SVG/PathSVG";
 
-const Selector = () => {
+const Selector = ({ handleSelectLocation }) => {
 	const list_location = [
 		"Hà Nội",
 		"Hồ Chí Minh",
@@ -10,7 +11,7 @@ const Selector = () => {
 	];
 	return (
 		<motion.ul
-			className="absolute left-0 z-10 grid w-full grid-rows-5 px-4 py-2 shadow-2xl -top-[312px] bg-main-bg rounded-2xl text-sub-text font-vietnam-pro"
+			className="absolute left-0 z-10 grid w-[200%] grid-rows-5 px-2 py-2 shadow-2xl -top-[320px] bg-main-bg rounded-2xl text-sub-text font-vietnam-pro"
 			initial={{ scale: 0, opacity: 0 }}
 			animate={{ scale: 1, opacity: 1 }}
 			exit={{ scale: 0, opacity: 0 }}
@@ -19,10 +20,18 @@ const Selector = () => {
 				return (
 					<li
 						key={location}
-						className={`px-2 py-4 rounded-xl ${
+						className={`px-2 py-3 rounded-xl ${
 							index === 3 && "bg-selection-active text-black"
-						} hover:bg-selection-active hover:text-black transition-colors`}
+						} hover:bg-selection-active group hover:text-black transition-colors flex items-center gap-x-2`}
+						onClick={() => handleSelectLocation(location)}
 					>
+						<div
+							className={`p-2 border-2 rounded-full border-selection-active group-hover:bg-main-bg ${
+								index === 3 && "bg-main-bg"
+							}`}
+						>
+							<PathSVG className="w-4 h-4"></PathSVG>
+						</div>
 						{location}
 					</li>
 				);

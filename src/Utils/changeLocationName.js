f91d3f2a5903519ff195bf14ccf3ee908c11e1b5
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-10-22 00:52:12 +0700

    Change something in UI of searchbar(font, padding), update logic focus when select by keyboard and update current index selection logic display

--- src/Utils/changeLocationName.js
+++ src/Utils/changeLocationName.js
@@ -1,5 +1,7 @@
-const changeLocationName = (location) => {
-	var name_location = location.name.replace("Tỉnh ", "");
+const changeLocationName = (location, isLocationObject = 1) => {
+	var name_location;
+	if (isLocationObject) name_location = location.name.replace("Tỉnh ", "");
+	else name_location = location.replace("Tỉnh ", "");
 	name_location = name_location.replace("Thành phố ", "");
 	name_location = name_location.replace("Huyện ", "");
 	name_location = name_location.replace("Thị xã ", "");

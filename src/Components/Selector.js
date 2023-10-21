19b792c31f7d356a2f05be68f5a031dd4e453c40
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-10-21 21:10:35 +0700

    Add logic search provinces depend on query input (update in future will have district of provinces), make search layout structure file, clean code, make logic UI of location tab and selector

--- src/Components/Selector.js
+++ src/Components/Selector.js
@@ -1,42 +1,46 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import PathSVG from "@/Assets/Icons/SVG/PathSVG";
+import getVietNamProvinces from "@/Utils/getVietNamProvinces";
 
-const Selector = ({ handleSelectLocation }) => {
-	const list_location = [
-		"Hà Nội",
-		"Hồ Chí Minh",
-		"Hải Phòng",
-		"Vũng Tàu",
-		"Đà Nẵng",
-	];
+const Selector = ({ handleSelectLocation, locationQuery, closeSelector }) => {
+	const locationDisplay = getVietNamProvinces(locationQuery);
 	return (
-		<motion.ul
-			className="absolute left-0 z-10 grid w-[200%] grid-rows-5 px-2 py-2 shadow-2xl -top-[320px] bg-main-bg rounded-2xl text-sub-text font-nunito font-semibold"
-			initial={{ scale: 0, opacity: 0 }}
-			animate={{ scale: 1, opacity: 1 }}
-			exit={{ scale: 0, opacity: 0 }}
-		>
-			{list_location.map((location, index) => {
-				return (
-					<li
-						key={location}
-						className={`px-2 py-3 rounded-xl ${
-							index === 3 && "bg-selection-active text-black"
-						} hover:bg-selection-active group hover:text-black transition-colors flex items-center gap-x-2`}
-						onClick={() => handleSelectLocation(location)}
-					>
-						<div
-							className={`p-2 border-2 rounded-full border-selection-active group-hover:bg-main-bg ${
-								index === 3 && "bg-main-bg"
-							}`}
-						>
-							<PathSVG className="w-4 h-4"></PathSVG>
-						</div>
-						{location}
-					</li>
-				);
-			})}
-		</motion.ul>
+		<AnimatePresence>
+			{locationDisplay.length > 0 && (
+				<motion.ul
+					className={`absolute left-0 z-10 grid w-[200%] grid-rows-5 px-2 py-2 shadow-2xl -top-[320px] bg-main-bg rounded-2xl text-sub-text font-nunito font-semibold cursor-default`}
+					initial={{ scale: 0, opacity: 0 }}
+					animate={{ scale: 1, opacity: 1 }}
+					exit={{ scale: 0, opacity: 0 }}
+					onBlur={closeSelector}
+					tabIndex="-1"
+				>
+					{locationDisplay.map((location, index) => {
+						var name_location = location.name.replace("Tỉnh ", "");
+						name_location = name_location.replace("Thành phố ", "");
+						name_location = name_location.replace("Huyện ", "");
+						return (
+							<li
+								key={location.name}
+								className={`px-2 py-3 rounded-xl ${
+									index === 0 && "bg-selection-active text-black"
+								} hover:bg-selection-active group hover:text-black transition-colors flex items-center gap-x-2 cursor-pointer`}
+								onClick={() => handleSelectLocation(name_location)}
+							>
+								<div
+									className={`p-2 border-2 rounded-full border-selection-active group-hover:bg-main-bg ${
+										index === 0 && "bg-main-bg"
+									}`}
+								>
+									<PathSVG className="w-4 h-4"></PathSVG>
+								</div>
+								{name_location}
+							</li>
+						);
+					})}
+				</motion.ul>
+			)}
+		</AnimatePresence>
 	);
 };
 
---
+++ src/Layouts/HomePage/SearchLayouts/SearchArea.js
@@ -0,0 +1,26 @@
+import homepage_bg from "@/Assets/Images/villa.jpg";
+import SearchBar from "@/Layouts/HomePage/SearchLayouts/SearchBar";
+
+const SearchArea = () => {
+	return (
+		<div className=" h-[550px] relative mt-8">
+			<div
+				className="absolute inset-0 z-0 bg-bottom bg-no-repeat bg-cover rounded-2xl brightness-[70%]"
+				style={{
+					backgroundImage: `url(${homepage_bg})`,
+				}}
+			></div>
+			<div className="absolute z-10 font-semibold text-white select-none font-vietnam-pro inset-y-32 inset-x-16">
+				<p className="text-[56px] ">
+					Khám phá, <br /> Trải nghiệm
+				</p>
+				<p className="text-[20px] font-medium ml-3">
+					Chọn đúng nơi, trải nghiệm tốt
+				</p>
+			</div>
+			<SearchBar></SearchBar>
+		</div>
+	);
+};
+
+export default SearchArea;

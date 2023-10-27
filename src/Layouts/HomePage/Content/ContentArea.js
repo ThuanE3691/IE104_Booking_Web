c2cdc7519ad22b8868703949d6b538909f41cc3d
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-10-27 22:09:39 +0700

    Added Sale Section

---
+++ src/Layouts/HomePage/Content/ContentArea.js
@@ -0,0 +1,11 @@
+import SaleSection from "./SaleSection";
+
+const ContentArea = () => {
+	return (
+		<div className="mt-40">
+			<SaleSection></SaleSection>
+		</div>
+	);
+};
+
+export default ContentArea;
---
+++ src/Layouts/HomePage/Content/SaleSection.js
@@ -0,0 +1,50 @@
+import sale_img1 from "@/Assets/Images/sale_img1.jpeg";
+import sale_img2 from "@/Assets/Images/sale_img2.jpeg";
+
+const SaleSection = () => {
+	return (
+		<section className="font-vietnam-pro">
+			<h3 className="content-header ">Ưu đãi</h3>
+			<h4 className="mt-2 font-normal">
+				Khuyến mãi, giảm giá và ưa đãi đặc biệt dành riêng cho bạn
+			</h4>
+			<div className="flex items-center gap-8 mt-4">
+				<div className="flex bg-white shadow-2xl rounded-xl">
+					<div className="pt-8 pb-2 pl-6 pr-16 w-[512px]">
+						<h4 className="text-lg font-semibold ">
+							Tận hưởng kỳ nghỉ dài nhất của bạn
+						</h4>
+						<p className="mt-1 text-sm leading-6 tracking-wide font-extralight">
+							Tìm kiếm các chỗ nghỉ cho phép lưu trú dài ngày với giá theo tháng
+							ưu đãi
+						</p>
+						<button className="px-5 py-2 mt-4 font-semibold text-white transition-colors bg-blue-600 rounded-lg hover:bg-slate-200 hover:text-black">
+							Tìm chỗ nghỉ
+						</button>
+					</div>
+					<img
+						src={sale_img1}
+						alt=""
+						className="w-[185px] aspect-square rounded-r-xl"
+					/>
+				</div>
+				<div
+					className=" h-[185px] w-2/4 bg-cover bg-no-repeat rounded-xl shadow-xl pl-6 pr-[15%] pt-8 pb-2 text-white"
+					style={{ backgroundImage: `url(${sale_img2})` }}
+				>
+					<h4 className="text-lg font-semibold ">
+						Tiết kiệm 15% với giá Ưu Đãi Cuối Năm
+					</h4>
+					<p className="mt-1 text-sm leading-6 tracking-wide font-extralight">
+						Khám phá hàng nghìn điểm đến khắp thế giới và tiết kiệm từ 15%
+					</p>
+					<button className="px-5 py-2 mt-4 font-semibold text-white transition-colors bg-blue-600 rounded-lg hover:bg-slate-200 hover:text-black">
+						Tìm Ưu Đãi Cuối Năm
+					</button>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default SaleSection;

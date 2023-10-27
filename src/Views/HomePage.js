d0190b5ecc725efc63435b22357437d9a6228cd7
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-10-27 20:29:54 +0700

    Init Content Layout

--- src/Views/HomePage.js
+++ src/Views/HomePage.js
@@ -1,11 +1,13 @@
 import Navbar from "@/Layouts/Navbar";
 import SearchArea from "@/Layouts/HomePage/SearchLayouts/SearchArea";
+import Content from "@/Layouts/HomePage/Content/Content";
 
 const HomePage = () => {
 	return (
 		<div className="w-full min-h-full px-32 py-8 bg-main-bg">
 			<Navbar></Navbar>
 			<SearchArea></SearchArea>
+			<Content></Content>
 		</div>
 	);
 };

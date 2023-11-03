8b7d7515955f33df670045c0a8496ef02b318055
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-11-03 18:22:30 +0700

    Set up Search Page

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -3,12 +3,13 @@ import config from "@/Config";
 
 // Page
 import Home from "@/Views/Home";
+import Search from "@/Views/Search";
 
 import { Fragment } from "react";
 
 const routes = [
 	{ path: config.routes.home, component: Home },
-	{ path: config.routes.search, component: Fragment },
+	{ path: config.routes.search, component: Search },
 	{ path: config.routes.hotel, component: Fragment },
 	{ path: config.routes.about, component: Fragment },
 	{ path: config.routes.contact, component: Fragment },
---
+++ src/Views/Search.js
@@ -0,0 +1,9 @@
+const Search = () => {
+	return (
+		<div className="flex flex-col w-full min-h-full px-32 bg-main-bg">
+			Search
+		</div>
+	);
+};
+
+export default Search;

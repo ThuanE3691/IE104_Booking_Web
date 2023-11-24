e8691032f32af2c37cee8438ad4641dcdda590d8
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-11-24 19:32:12 +0700

    Change UI of PopUp Card

--- src/Layouts/Shared/Map/PopupCard.js
+++ src/Layouts/Shared/Map/PopupCard.js
@@ -1,4 +1,7 @@
 import { Popup } from "react-leaflet";
+import { FaLocationDot } from "react-icons/fa6";
+import formatNumber from "@/Utils/formatNumber";
+
 import "@/css/Map/map.css";
 
 const PopupCard = ({ hotel, ...properties }) => {
@@ -7,13 +10,15 @@ const PopupCard = ({ hotel, ...properties }) => {
 			<img
 				src={hotel.max_1440_photo_url}
 				alt=""
-				className=" w-[200px] rounded-l-lg"
+				className=" w-[300px] h-[220px] rounded-[30px_30px_0_0] z-[1]"
 			/>
-			<section className="w-full px-4 py-4 font-vietnam-pro">
+			<div className="absolute z-10 w-full h-[30px] left-0 top-[200px] bg-white rounded-tr-[30px]"></div>
+
+			<section className="flex flex-col w-full px-4 py-4 font-vietnam-pro">
 				<h3 className="text-base font-semibold bg-white w-fit">
 					{hotel.hotel_name_trans}
 				</h3>
-				<div className="flex items-center justify-start mt-2 gap-x-2 w-fit">
+				{/* <div className="flex items-center justify-start mt-2 gap-x-2 w-fit">
 					<div className="p-2 font-semibold text-white rounded-[10px_10px_10px_0px] bg-button-primary w-fit text-xs">
 						{hotel.review_score.toFixed(1)}
 					</div>
@@ -23,7 +28,16 @@ const PopupCard = ({ hotel, ...properties }) => {
 						</span>
 						<span className="text-xs">{hotel.review_nr} đánh giá</span>
 					</div>
-				</div>
+				</div> */}
+				<span className="flex items-center mt-2 gap-x-1">
+					<FaLocationDot></FaLocationDot> {hotel.district}, {hotel.city_trans}
+				</span>
+				<span className="self-end justify-end mt-10 text-lg font-semibold text-text-primary">
+					{formatNumber(
+						hotel.composite_price_breakdown.gross_amount_per_night.value
+					)}{" "}
+					VND/ đêm
+				</span>
 			</section>
 		</Popup>
 	);
